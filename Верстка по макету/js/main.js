d5f143bb07fa931e3d8432d3906da12534e07514
AnastasiaKCH <nastena7417@mail.ru>
2023-06-14 00:19:25 +0300

    new project

---
+++ Верстка по макету/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $(".block2").slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        variableWidth: true,
+        prevArrow: '<div class="left"><i class="leftArrow"></i></div>',
+        nextArrow: '<div class="right"><i class="rightArrow"></i></div>'
+    })
+});
+
+
+
